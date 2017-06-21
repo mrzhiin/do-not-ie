683977dc16c57c7268a15cf8bf795ad2255df989
MrZhiin <mrzhiin@gmail.com>
2017-06-21 14:08:35 +0800

    添加文件

---
+++ README.md
@@ -0,0 +1,16 @@
+# IE 兼容性弹窗
+
+该弹窗特别为国内制作，推荐浏览器包含了 360浏览器、QQ浏览器、Google Chrome、Mozilla Firefox
+
+由于 Google Chrome 官网链接不稳定，所以采用了新浪的下载地址
+
+# 使用
+
+在 ```head``` 标签中添加 js 链接，并包含在 IE 的条件注释里
+
+```
+<!--[if lte IE 9]>
+<script src="js/js.min.js"></script>
+<![endif]-->
+``` 
+将 ```dnie```文件夹放在```html```文件同级目录
---
+++ js/dnie.js
@@ -0,0 +1,53 @@
+;(function () {
+    window.onload = function () {
+        var html =
+            '<div style="position:fixed;z-index:9999;top:0;right:0;bottom:0;left:0;height:100%;width:100%;background:rgba(0,0,0,0.5);filter: progid:DXImageTransform.Microsoft.gradient(startcolorstr=#7F000000,endcolorstr=#7F000000);">' +
+            '<div style="margin:64px auto auto;padding:0 16px 24px;width:600px;border:solid 1px #ddd;background-color:#fff;text-align:center;color:#333;letter-spacing:1px;line-height:1.6em;font-size:14px;">' +
+            '<div style="font-size:20px;font-weight:bold;margin-top:32px;">您似乎正在使用IE浏览器</div>' +
+            '<div style="font-size:18px;margin-top:16px;">为了保障您的安全，请更换使用以下浏览器</div>' +
+            '<div style="margin-top:24px;">' +
+            '<div style="width:25%;float:left;text-align:center">' +
+            '<img src="dnie/360.jpg">' +
+            '<div>360 浏览器</div>' +
+            '<a href="http://se.360.cn/" style="display:block;width:4em;padding:4px;margin:8px auto auto;background-color:#e74c3c;color:#fff;text-decoration: none;">下载</a>' +
+            '</div>' +
+            '<div style="width:25%;float:left;text-align:center">' +
+            '<img src="dnie/qq.jpg">' +
+            '<div>QQ 浏览器</div>' +
+            '<a href="http://browser.qq.com/" style="display:block;width:4em;padding:4px;margin:8px auto auto;background-color:#e74c3c;color:#fff;text-decoration: none;">下载</a>' +
+            '</div>' +
+            '<div style="width:25%;float:left;text-align:center">' +
+            '<img src="dnie/firefox.jpg">' +
+            '<div>Mozilla Firefox</div>' +
+            '<a href="https://www.mozilla.org/zh-CN/firefox/new/" style="display:block;width:4em;padding:4px;margin:8px auto auto;background-color:#e74c3c;color:#fff;text-decoration: none;">下载</a>' +
+            '</div>' +
+            '<div style="width:25%;float:left;text-align:center">' +
+            '<img src="dnie/chrome.jpg">' +
+            '<div>Google Chrome</div>' +
+            '<a href="http://down.tech.sina.com.cn/content/40975.html" style="display:block;width:4em;padding:4px;margin:8px auto auto;background-color:#e74c3c;color:#fff;text-decoration: none;">下载</a>' +
+            '</div>' +
+            '<div style="clear:both;float:none;"></div>' +
+            '</div>' +
+            '<div style="height:1px;background-color:#ddd;margin-top:32px;font-size:0;"></div>' +
+            '<div>' +
+            '<div style="font-size:20px;font-weight:bold;margin-top:32px;">如果您正在使用<span style="padding:4px;margin:0 4px;background-color:#2ecc71;color:#fff;">360浏览器</span>或<span style="padding:4px;margin:0 4px;background-color:#3498db;color:#fff;">QQ浏览器</span></div>' +
+            '<div style="font-size:18px;margin-top:16px;">请按照以下步骤，以获得更好的体验</div>' +
+            '</div>' +
+            '<div style="margin-top:32px;font-size:16px;">' +
+            '<div style="float:left;width:50%;line-height:2em;">' +
+            '<div>1.点击地址栏闪电图标</div>' +
+            '<div>2.选择第一项极速模式</div>' +
+            '</div>' +
+            '<div style="float:left;width:50%;">' +
+            '<img src="dnie/tip.jpg">' +
+            '</div>' +
+            '<div style="clear:both;float:none;"></div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+        var body = document.body;
+        var dom = document.createElement('div');
+        body.appendChild(dom)
+        dom.outerHTML = html;
+    }
+})();
